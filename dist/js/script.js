a13fa171357dc48184edcdf1545266aff5f41a2b
CyberFrontEnd <mail.techcd.ru>
2024-04-28 16:37:52 +0400

    Modif page

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   const swiper = new Swiper(".mySwiper", {
-    cssMode: true,
+    lazy: true,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
@@ -9,7 +9,18 @@ $(document).ready(function () {
       el: ".swiper-pagination",
       clickable: true,
     },
-    mousewheel: true,
-    keyboard: true,
+  });
+
+
+  const reviewsBlockSlider = new Swiper(".reviewsBlockSlider", {
+    lazy: true,
+    navigation: {
+      nextEl: ".swiper-button-next2",
+      prevEl: ".swiper-button-prev2",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
   });
 });
